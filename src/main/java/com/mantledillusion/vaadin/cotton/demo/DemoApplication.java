8e910e5d5718dab878a373a6e3570a723f64ef8f
Tobias Weber <weber.tobias@live.de>
2019-05-09 18:55:17 +0200

    setup for Hura-WebLaunch

---
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.mantledillusion.vaadin.cotton.demo;
+
+import com.mantledillusion.injection.hura.weblaunch.HuraWeblaunchApplication;
+import io.undertow.server.handlers.resource.ClassPathResourceManager;
+import io.undertow.server.handlers.resource.ResourceManager;
+
+public class DemoApplication {
+
+    private static final ResourceManager VAADIN_RESOURCE_MANAGER =
+            new ClassPathResourceManager(DemoApplication.class.getClassLoader(), "META-INF/resources/");
+
+    public static void main(String[] args) {
+        HuraWeblaunchApplication
+                .build(DemoApplicationInitializer.class)
+                .setResourceManager(VAADIN_RESOURCE_MANAGER)
+                .startUp(args);
+    }
+}
---
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/DemoApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.mantledillusion.vaadin.cotton.demo;
+
+import com.mantledillusion.injection.hura.core.annotation.instruction.Define;
+import com.mantledillusion.injection.hura.web.HuraWebApplicationInitializer;
+import com.mantledillusion.injection.hura.web.env.WebEnvironmentFactory;
+import com.mantledillusion.vaadin.cotton.CottonEnvironment;
+import com.mantledillusion.vaadin.cotton.CottonServlet;
+
+public class DemoApplicationInitializer implements HuraWebApplicationInitializer {
+
+    @Define
+    public SingletonAllocation defineCottonServlet() {
+        return WebEnvironmentFactory
+                .registerServlet(CottonServlet.class)
+                .addMapping("/*")
+                .build();
+    }
+
+    @Define
+    public PropertyAllocation defineActivateAutomaticRouteDiscovery() {
+        return CottonEnvironment.forAutomaticRouteDiscovery(true);
+    }
+}
