50afb59dc79d2b8a4ebda811ff166e69e9a83903
tobias.weber <tobias.weber@ecotel.de>
2019-06-11 15:06:21 +0200

    Base Setup for all demo branches

--- src/main/java/com/mantledillusion/vaadin/cotton/demo/DemoView.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/DemoView.java
@@ -1,23 +1,9 @@
 package com.mantledillusion.vaadin.cotton.demo;
 
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
 @Route("demo")
-public class DemoView extends VerticalLayout {
+public class DemoView extends Div {
 
-    public DemoView() {
-        setSizeFull();
-
-        HorizontalLayout columnLayout = new HorizontalLayout();
-        columnLayout.setHeightFull();
-        add(columnLayout);
-        setHorizontalComponentAlignment(Alignment.CENTER, columnLayout);
-
-        Label helloLabel = new Label("Hello from your first Cotton app!");
-        columnLayout.add(helloLabel);
-        columnLayout.setVerticalComponentAlignment(Alignment.CENTER, helloLabel);
-    }
 }
